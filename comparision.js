8b07527037be0601ef1bb03bf02d5293fe6d2cd0
abushamacd <assiddik001@gmail.com>
2026-08-09 11:58:00 +0600

    Comperision

---
+++ comparision.js
@@ -0,0 +1,20 @@
+/*
+Comparision Operator
+1. Greater than >
+2. Less than <
+3. Equal ==
+4. greater than or equal >=
+5. less than or equal <=
+6. Not Equal != 
+7. And &&
+8. Or ||
+
+*/
+
+let mark = 80;
+
+if (mark >= 80) {
+  console.log("Got A+");
+} else {
+  console.log("Under A+");
+}

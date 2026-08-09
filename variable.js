9b33edcac8281773cda9b3ba1806a7fd088ad29c
abushamacd <assiddik001@gmail.com>
2026-08-09 10:45:37 +0600

    variables

---
+++ variable.js
@@ -0,0 +1,14 @@
+/**
+ Naming Convension
+ 1. Single word
+ 2. no quote
+ 3. no gap or dash
+ 4. can not start with number
+ 5. do not use reserve keyword
+ 6. Prefer camel case
+ */
+
+let fullName = "Abu Shama";
+console.log(nafullNameme);
+let age = 27;
+console.log(age);

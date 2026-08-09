4b8430b6f8f14af0a040d79049ba030c4f9dbf5d
abushamacd <assiddik001@gmail.com>
2026-08-09 10:51:02 +0600

    mathmatical operation

---
+++ math.js
@@ -0,0 +1,12 @@
+let num1 = 12;
+let num2 = 5;
+
+console.log(num1 + num2); // Addition
+console.log(num1 - num2); // Substruction
+console.log(num1 * num2); // Multipication
+console.log(num1 / num2); // Division
+console.log(num1 % num2); // Modulas
+
+// short hand
+let age = 22;
+console.log((age += 1));

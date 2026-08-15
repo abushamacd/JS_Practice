f08a82ff8104b9fc009033785076a0af86e5bf74
abushamacd <assiddik001@gmail.com>
2026-08-15 11:26:42 +0600

    arr push, pop, unshift, shift

--- array.js
+++ array.js
@@ -3,6 +3,11 @@ const numbers = [10, 25, 42, 67, 99];
 console.log(numbers.length);
 console.log(numbers.indexOf(25)); // find the index of an element
 console.log(numbers[2]); // get by index number
-console.log(numbers);
 numbers[2] = 0; // update element by index
+numbers.push(111); // add element last
+// console.log(numbers);
+numbers.pop(); // remove element last
+numbers.unshift(111); // add element first
+// console.log(numbers);
+numbers.shift(); // remove element first
 console.log(numbers);

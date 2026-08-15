3757303626851544431edb9fd8ee2cbc536aa7eb
abushamacd <assiddik001@gmail.com>
2026-08-15 11:17:39 +0600

    arr lenght, index, set & get by index

---
+++ array.js
@@ -0,0 +1,8 @@
+const numbers = [10, 25, 42, 67, 99];
+
+console.log(numbers.length);
+console.log(numbers.indexOf(25)); // find the index of an element
+console.log(numbers[2]); // get by index number
+console.log(numbers);
+numbers[2] = 0; // update element by index
+console.log(numbers);

89d678c193aec12ffba4da8ab1497b80fa46dafa
abushamacd <assiddik001@gmail.com>
2026-08-19 15:34:31 +0600

    upper case, lower case, trim

---
+++ string.js
@@ -0,0 +1,8 @@
+// string immutable -> not change
+
+const subject = "Math ";
+const book = "math";
+
+console.log(subject.toLowerCase());
+console.log(book.toUpperCase());
+console.log(subject.trim() + "test");

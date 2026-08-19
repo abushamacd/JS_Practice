bc499ac8f09ea544ecb33560db71420d23729b04
abushamacd <assiddik001@gmail.com>
2026-08-19 16:00:22 +0600

    split, join, includes

--- string.js
+++ string.js
@@ -6,3 +6,13 @@ const book = "math";
 console.log(subject.toLowerCase());
 console.log(book.toUpperCase());
 console.log(subject.trim() + "test");
+
+const address = "Chuadanga";
+console.log(address.slice(4, 9));
+console.log(address.slice(4));
+
+const friends = "Rahim, Karim, Jamil, Salma";
+const friendsarr = friends.split(", ");
+console.log(friendsarr);
+console.log(friendsarr.includes("Rahim"));
+console.log(friendsarr.join("- "));
